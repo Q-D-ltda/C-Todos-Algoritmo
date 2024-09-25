7cde6229b2e63175c37e57ae15cf0f0344e6ad80
Matheus Henrique Pradal Souza <matheushenriquepradal@gmail.com>
2024-09-25 20:05:36 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,87 @@
+/*estruturas de repetição
+
+laço ou loop.*/
+
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+/*int main() {
+    int i = 1;  
+    int num;
+    while (i <= 5) {
+        printf("Digite o numero %d: ", i);
+        scanf("%d", &num); 
+
+      
+        printf("O número %d multiplicado por 3 e: %d\n", num, num * 3);
+
+        i++; 
+    }
+    return 0;
+}*/
+
+
+
+/*int main() {
+    int num;
+    char resposta;
+
+    do {
+        // Leitura do valor numérico
+        printf("Digite um número: ");
+        scanf("%d", &num);
+
+        // Multiplicação por 3 e apresentação do resultado
+        printf("O número %d multiplicado por 3 é: %d\n", num, num * 3);
+
+        // Pergunta se o usuário deseja realizar outro cálculo
+        printf("Deseja realizar um novo cálculo? (s/n): ");
+        scanf(" %c", &resposta);  // Note o espaço antes de %c para ignorar o Enter anterior
+
+    } while (resposta == 's' || resposta == 'S');  // Continua enquanto a resposta for 's' ou 'S'
+
+    printf("Programa encerrado.\n");
+
+    return 0;
+}*/
+
+int main(){
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

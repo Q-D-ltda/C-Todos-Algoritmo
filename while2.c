911c4c958a708896ada55a2c652645cd21df3bc9
Jau9090 <jpmarinhoh@gmail.com>
2024-09-25 20:51:54 -0300

    Update while2.c

--- while2.c
+++ while2.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-
 int main() {
-    int count, num;
+    int num, somaResultados = 0;
     char resposta;
-    count = 1;
 
-    printf("Deseja realizar uma multiplicação? s/n: ");
-    scanf(" %c", &resposta);
+    resposta = 's';
 
-    while (resposta = 's') {
-    printf("Digite um valor para ser multiplicado por 3: ");
-    scanf("%d", &num);
+    while (resposta == 's') {
+        printf("Digite um valor para ser multiplicado por 3: ");
+        scanf("%d", &num);
 
-    printf("%d * 3 = %d \n", num, (num * 3));
-    count--;
+        int resultado = num * 3; // Calcula o resultado da multiplicação
+        printf("%d * 3 = %d \n", num, resultado);
 
-    printf("Deseja realizar outra multiplicação? s/n: ");
-    scanf(" %c", &resposta);
+        somaResultados += resultado; // Adiciona o resultado à soma total
+
+        printf("Deseja realizar outra multiplicação? s/n: ");
+        scanf(" %c", &resposta);
     }
+
+    printf("A soma total dos resultados é: %d\n", somaResultados);
 
     return 0;
 }
